b9ba0a338b51c82063a7e192f875a5c2c0fd3c89
Kautenja <kautencreations@gmail.com>
2018-03-30 17:35:37 -0500

    duumy DQN

--- src/agents/deep_q_agent.py
+++ src/agents/deep_q_agent.py
@@ -1,12 +1,16 @@
 """An implementation of Deep Q-Learning."""
+import numpy as np
+import cv2
 from keras.models import Model
 from .agent import Agent
+from .replay_queue import ReplayQueue
 
 
 # the format string for this objects representation
 _REPR_TEMPLATE = """
 {}(
     model={},
+    env={},
     learning_rate={},
     discount_factor={},
     exploration_rate={},
@@ -40,6 +44,7 @@ class DeepQAgent(Agent):
 
     def __init__(self,
                  model: Model,
+                 env,
                  learning_rate: float=0.001,
                  discount_factor: float=0.99,
                  exploration_rate: float=0.8,
@@ -60,6 +65,9 @@ class DeepQAgent(Agent):
         # verify model
         if not isinstance(model, Model):
             raise TypeError('model must be of type: keras.models.Model')
+
+        # TODO: validate env type
+
         # verify learning_rate
         if not isinstance(learning_rate, float):
             raise TypeError('learning_rate must be of type float')
@@ -87,17 +95,22 @@ class DeepQAgent(Agent):
             raise ValueError('episodes must be positive')
         # assign args to self
         self.model = model
+        self.env = env
         self.learning_rate = learning_rate
         self.discount_factor = discount_factor
         self.exploration_rate = exploration_rate
         self.exploration_decay = exploration_decay
         self.episodes = episodes
+        # setup other instance members
+        # TODO: parameterize the replay queue size (defaults to 20000)
+        self.queue = ReplayQueue()
 
     def __repr__(self) -> str:
         """Return a debugging string of this agent."""
         return _REPR_TEMPLATE.format(
             self.__class__.__name__,
             self.model,
+            self.env,
             self.learning_rate,
             self.discount_factor,
             self.exploration_rate,
@@ -105,18 +118,149 @@ class DeepQAgent(Agent):
             self.episodes,
         )
 
-    def step(self):
+    @property
+    def num_actions(self) -> int:
+        """Return the number of actions for this agent."""
+        return self.model.output_shape[1]
+
+    def predict_action(self, frames: list) -> tuple:
         """
-        TODO: description
-        TODO: args and types
-        TODO: return type
-        TODO: implementation
+        Predict an action from a stack of frames.
+
+        Args:
+            frames: the stack of frames to predict Q values from
+
+        Returns:
+            a tuple of:
+                - the optimal action
+                - the Q value for the optimal action
+
         """
-        # TODO: this is just example code
-        # take a step and observe the environment variables
-        next_state, reward, done, info = env.step(action)
-        # calculate the target: r + γ max_a'(Q[s', a'])
-        target = reward + gamma * np.argmax(model.predict(next_state))
+        # predict the values of each action
+        actions = self.model.predict(
+            frames.reshape((1, *self.model.input_shape[1:])),
+            batch_size=1
+        )
+        # draw a number in [0, 1] and explore if it's less than the
+        # exploration rate
+        if np.random.random() < self.exploration_rate:
+            # select a random action. the output shape of the network implies
+            # the action name by index, so use that shape as the upper bound
+            optimal_action = np.random.randint(0, self.num_actions)
+        else:
+            # select the action with the highest estimated score as the
+            # optimal action
+            optimal_action = np.argmax(actions)
+
+        # return the optimal action and its corresponding Q value
+        return optimal_action, actions[0, optimal_action]
+
+    def train(self, s, a, r, d, s2) -> None:
+        """
+        """
+        targets = np.zeros((s.shape[0], self.num_actions))
+
+        for index in range(len(s)):
+            targets[index] = self.model.predict(
+                s[index].reshape(self.model.input_shape),
+                batch_size=1
+            )
+            a_prime = self.model.predict(
+                s2[index].reshape(self.model.input_shape),
+                batch_size=1
+            )
+            targets[i, a[i]] = r[i]
+            if not d[i]:
+                targets[i, a[i]] += self.discount_factor * np.max(a_prime)
+
+        return self.model.train_on_batch(s, targets)
+
+    def downsample(self, frame):
+        """
+        Down-sample the given frame from RGB to B&W with a reduced size.
+
+        Args:
+            frame: the frame to down-sample
+
+        Returns:
+            a down-sample B&W frame
+
+        """
+        return cv2.resize(cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY), (84, 84))
+
+    @property
+    def initial_state(self):
+        """
+        """
+        # reset the environment
+        state = [self.env.reset()] * 4
+        # convert the initial frame to 4 down-sampled frames
+        black_buffer = np.array([self.downsample(frame) for frame in state]).T
+        return black_buffer
+        # sample a random action to start with
+        # action = self.env.action_space.sample()
+        # TODO: refactor this four out to be parameterized somehow, maybe
+        # build_model will have to be passed or something
+        # for index in range(4):
+        #     next_state, reward, done, info = env.step(action=action)
+        # return [self.env.step(action=action) for index in range(4)]
+
+    def next_state(self, action):
+        """
+        """
+        next_state = []
+        reward = 0
+        # iterate over the number of buffered frames
+        for _ in range(4):
+            # render the frame
+            self.env.render()
+            # make the step and observe the state, reward, done
+            state, _reward, done, _ = self.env.step(action=action)
+            # store the state and reward from this frame
+            next_state.append(self.downsample(state))
+            reward += _reward
+            # TODO: is this necessary?
+            reward = reward if not done else -10
+
+        next_state = np.array(next_state).T
+
+        return next_state, reward / 4, done
+
+    def run(self):
+        """
+        """
+        for episode in range(self.episodes):
+            # reset the game and get the initial state
+            state = self.initial_state
+            # the done flag indicating that an episode has ended
+            done = False
+            # loop until done
+            while not done:
+                # predict the best action based on the current state
+                action, Q = self.predict_action(state)
+                # hold the action for the number of frames
+                for _ in range(4):
+                    # render the frame
+                    self.env.render()
+                    # make the step and observe the next_state, reward, done
+                    next_state, reward, done, _ = self.env.step(action=action)
+                    # TODO: is this necessary
+                    reward = reward if not done else -10
+                    # push this state to the replay queue
+                    self.queue.push(state, action, reward, done, next_state)
+
+    # def step(self):
+    #     """
+    #     TODO: description
+    #     TODO: args and types
+    #     TODO: return type
+    #     TODO: implementation
+    #     """
+    #     # TODO: this is just example code
+    #     # take a step and observe the environment variables
+    #     next_state, reward, done, info = env.step(action)
+    #     # calculate the target: r + γ max_a'(Q[s', a'])
+    #     target = reward + gamma * np.argmax(model.predict(next_state))
 
 
 # explicitly define the outward facing API of this module
