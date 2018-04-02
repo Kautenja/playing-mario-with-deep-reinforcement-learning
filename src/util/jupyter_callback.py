9f9816efe8e1c55a33ef0182361342c2c1bb6111
Kautenja <kautencreations@gmail.com>
2018-04-02 00:46:40 -0500

    update Jupyter callback
    cleaner plots

--- src/util/jupyter_callback.py
+++ src/util/jupyter_callback.py
@@ -1,13 +1,15 @@
 """A rich reward tracking callback for Jupyter notebooks."""
 from matplotlib import pyplot as plt
+from matplotlib.ticker import MaxNLocator
 from IPython import display
 
 
 class JupyterCallback(object):
     """A rich reward tracking callback for Jupyter notebooks."""
 
-    def __init__(self) -> None:
+    def __init__(self, max_losses: int=10) -> None:
         """Create a new Jupyter Callback method."""
+        self.max_losses = max_losses
         # setup instance members
         self.scores = []
         self.losses = []
@@ -27,16 +29,27 @@ class JupyterCallback(object):
         # append the score to the list
         self.scores.append(score)
         self.losses.append(loss)
+        # MARK: score
+        plt.figure(figsize=(14,5))
         # plot the score
         plt.subplot(2, 1, 1)
         plt.plot(self.scores)
         plt.xlabel('Episode')
         plt.ylabel('Score')
+        # force integer axis tick labels
+        plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))
+        # MARK: loss
+        # keep the losses bounded to a certain size
+        losses = self.losses
+        if len(losses) > self.max_losses:
+            losses = losses[-self.max_losses:]
         # plot the loss
         plt.subplot(2, 1, 2)
-        plt.plot(self.losses)
-        plt.xlabel('Episode')
+        plt.plot(losses)
+        plt.xlabel('Last {} Episode(s)'.format(self.max_losses))
         plt.ylabel('Loss')
+        # force integer axis tick labels
+        plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))
         # adjust the layout
         plt.tight_layout()
         # clear the Jupyter front-end and send the new plot
