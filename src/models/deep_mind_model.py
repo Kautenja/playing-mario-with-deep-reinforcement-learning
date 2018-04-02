92049b85e1546287dc7aca5413a484fc4e182fde
Kautenja <ckfour@icloud.com>
2018-04-02 17:53:11 -0500

    update DQN model

--- src/models/deep_mind_model.py
+++ src/models/deep_mind_model.py
@@ -1,12 +1,13 @@
 """The Deep Mind Convolutional Neural Network (CNN) model."""
 from keras.models import Model
-from keras.models import Sequential
+from keras.layers import Input
 from keras.layers import Lambda
 from keras.layers import Dense
 from keras.layers import Flatten
 from keras.layers import Activation
+from keras.layers import Multiply
 from keras.layers.convolutional import Conv2D
-from keras.optimizers import Adam
+from keras.optimizers import RMSprop
 
 
 def build_deep_mind_model(
@@ -36,22 +37,31 @@ def build_deep_mind_model(
         a blank DeepMind CNN for image classification in a reinforcement agent
 
     """
-    # build the model for image classification fitting the given parameters
-    model = Sequential([
-        Lambda(lambda x: x / 255.0, input_shape=(*image_size, num_frames)),
-        Conv2D(32, (8, 8), strides=(4,4), padding='same'),
-        Activation('relu'),
-        Conv2D(64, (4, 4), strides=(2,2), padding='same'),
-        Activation('relu'),
-        Conv2D(64, (3, 3), strides=(1,1), padding='same'),
-        Activation('relu'),
-        Flatten(),
-        Dense(512),
-        Activation('relu'),
-        Dense(num_actions),
-    ])
+    # build the CNN using the functional API
+    cnn_input = Input((*image_size, num_frames), name='cnn')
+    cnn = Lambda(lambda x: x / 255.0)(cnn_input)
+    cnn = Conv2D(32, (8, 8), strides=(4,4), padding='same')(cnn)
+    cnn = Activation('relu')(cnn)
+    cnn = Conv2D(64, (4, 4), strides=(2,2), padding='same')(cnn)
+    cnn = Activation('relu')(cnn)
+    cnn = Conv2D(64, (3, 3), strides=(1,1), padding='same')(cnn)
+    cnn = Activation('relu')(cnn)
+    cnn = Flatten()(cnn)
+    cnn = Dense(512)(cnn)
+    cnn = Activation('relu')(cnn)
+    cnn = Dense(num_actions)(cnn)
+    # build the mask using the functional API
+    mask_input = Input((num_actions,), name='mask')
+    # put the two pieces of the graph together
+    output = Multiply()([cnn, mask_input])
+
+    # build the model
+    model = Model(input=[cnn_input, mask_input], output=output)
     # compile the model with the default loss and optimization technique
-    model.compile(loss='mse', optimizer=Adam(lr=learning_rate))
+    # TODO: parameterize optimizer, is learning rate necessary?
+    # model.compile(loss='mse', optimizer=Adam(lr=learning_rate))
+    optimizer = RMSprop(lr=0.00025, rho=0.95, epsilon=0.01)
+    model.compile(loss='mse', optimizer=optimizer)
 
     return model
 
