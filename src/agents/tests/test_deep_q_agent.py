60515bed3b0b0df58b827851773951ac476fe816
Kautenja <ckfour@icloud.com>
2018-04-01 22:58:52 -0500

    fix tests

--- src/agents/tests/test_deep_q_agent.py
+++ src/agents/tests/test_deep_q_agent.py
@@ -1,17 +1,17 @@
 """Unit tests for the DeepQAgent class."""
 from unittest import TestCase
-from src.models import build_deep_mind_model
+import gym
 from ..deep_q_agent import DeepQAgent
 
 
 class DeepQAgent_init(TestCase):
     def test(self):
-        arb = DeepQAgent(build_deep_mind_model())
+        arb = DeepQAgent(gym.make('SpaceInvaders-v0'))
         self.assertIsInstance(arb, object)
 
 
 class DeepQAgent_is_subclass(TestCase):
     def test(self):
-        arb = DeepQAgent(build_deep_mind_model())
+        arb = DeepQAgent(gym.make('SpaceInvaders-v0'))
         from ..agent import Agent
         self.assertIsInstance(arb, Agent)
