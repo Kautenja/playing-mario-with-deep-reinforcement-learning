33ec31546c3b2b857ed45b4d8559fbdb36daa44d
Kautenja <ckfour@icloud.com>
2018-04-07 11:48:56 -0500

    create a NumPy based replay queue

---
+++ src/agents/replay_queue_np.py
@@ -0,0 +1,81 @@
+"""A queue for storing previous experiences to sample from."""
+import numpy as np
+
+
+class ReplayQueue(object):
+    """A queue for storing previous experiences to sample from."""
+
+    def __init__(self, size: int=1000000) -> None:
+        """
+        Initialize a new replay buffer with a given size.
+
+        Args:
+            size: the maximum number of experiences to store
+
+        Returns:
+            None
+
+        """
+        # verify size
+        if not isinstance(size, int):
+            raise TypeError('size must be of type int')
+        if size < 1:
+            raise ValueError('size must be at least 1')
+        # assign size to self
+        self._size = size
+
+    def __repr__(self) -> str:
+        """Return an executable string representation of self."""
+        return '{}(size={})'.format(self.__class__.__name__, self.size)
+
+    # def __len__(self) -> int:
+    #     """Return the number of items in the queue."""
+    #     return len(self.queue)
+
+    @property
+    def size(self) -> int:
+        """Return the size of the queue."""
+        return self._size
+
+    def push(self, s, a, r, d, s2) -> None:
+        """
+        Push a new experience onto the queue.
+
+        Args:
+            s: the current state
+            a: the action to get from state to next state
+            r: the reward as a result of the action
+            d: a flag indicating if the episode (game) has ended
+            s2: the next state as a result of the action from state
+
+        Returns:
+            None
+
+        """
+        # ensure types are the smallest possible before storing in the queue
+        s = s.astype('uint8')
+        s2 = s2.astype('uint8')
+        a = int(a)
+        r = int(r)
+        # push the variables onto the queue
+        self.queue.append((s, a, r, d, s2))
+
+    def sample(self, size: int=32):
+        """
+        Return a random sample of items from the queue.
+
+        Args:
+            size: the number of items to sample and return
+
+        Returns:
+            A random sample from the queue sampled uniformly
+
+        """
+        # generate an index of items to extract
+        idx_batch = set(np.random.randint(0, len(self), size))
+        # extract the batch from the queue
+        return [val for i, val in enumerate(self.queue) if i in idx_batch]
+
+
+# explicitly define the outward facing API of this module
+__all__ = ['ReplayQueue']
