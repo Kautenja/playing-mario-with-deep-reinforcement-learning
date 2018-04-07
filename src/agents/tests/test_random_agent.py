f37169ab4a5a7eb0ebaf3476fa5add4ec2b29929
Kautenja <ckfour@icloud.com>
2018-04-07 11:38:26 -0500

    update random agent tests

--- src/agents/tests/test_random_agent.py
+++ src/agents/tests/test_random_agent.py
@@ -1,16 +1,17 @@
 """Unit tests for the RandomAgent class."""
+import gym
 from unittest import TestCase
 from ..random_agent import RandomAgent
 
 
 class RandomAgent_init(TestCase):
     def test(self):
-        arb = RandomAgent()
+        arb = RandomAgent(gym.make('SpaceInvaders-v0'))
         self.assertIsInstance(arb, object)
 
 
 class RandomAgent_is_subclass(TestCase):
     def test(self):
-        arb = RandomAgent()
+        arb = RandomAgent(gym.make('SpaceInvaders-v0'))
         from ..agent import Agent
         self.assertIsInstance(arb, Agent)
