b4bd79e2ce8220d8934a0302974f27728b5254fa
Kautenja <ckfour@icloud.com>
2018-04-11 18:30:06 -0500

    update model init

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,6 +1,10 @@
 """Various deep learning models for value function estimation in deep RL."""
 from .deep_mind_model import build_deep_mind_model
+from .a3c_model import build_a3c_model
 
 
 # explicitly define the outward facing API for this package
-__all__ = ['build_deep_mind_model']
+__all__ = [
+    build_deep_mind_model.__name__,
+    build_a3c_model.__name__
+]
