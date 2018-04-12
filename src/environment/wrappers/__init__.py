06657818e71837a01f760d2f4a7b42cd4f30fd89
Kautenja <kautencreations@gmail.com>
2018-04-12 09:31:54 -0500

    fix penalized to penalize

--- src/environment/wrappers/__init__.py
+++ src/environment/wrappers/__init__.py
@@ -11,7 +11,7 @@ DeepMind functionality.
 from .clip_reward_env import ClipRewardEnv
 from .fire_reset_env import FireResetEnv
 from .noop_reset_env import NoopResetEnv
-from .penalized_death_env import PenalizeDeathEnv
+from .penalize_death_env import PenalizeDeathEnv
 from .downsample_env import DownsampleEnv
 from .frame_stack_env import FrameStackEnv
 
