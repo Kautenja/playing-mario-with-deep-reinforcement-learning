a4f7562ce7b468e67c923129fc3b1b98b34b44a7
Kautenja <kautencreations@gmail.com>
2018-04-12 17:09:15 -0500

    update DQN to use new replay queue

--- src/agents/deep_q_agent.py
+++ src/agents/deep_q_agent.py
@@ -33,7 +33,7 @@ class DeepQAgent(Agent):
     """The Deep Q reinforcement learning algorithm."""
 
     def __init__(self, env, render_mode: str='rgb_array',
-        replay_memory_size: int=250000,
+        replay_memory_size: int=750000,
         discount_factor: float=0.99,
         update_frequency: int=4,
         optimizer=Adam(lr=2e-5),
