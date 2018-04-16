0be4f63754dd01ba62baf936da7f2e28131bd147
Ashish Gupta <miu0198@owens-rw01.ten.osc.edu>
2018-04-16 19:41:57 -0400

    10M frames

---
+++ ipynb/results/Breakout/DeepQAgent/2018-04-16_01-56/agent.py
@@ -0,0 +1,12 @@
+DeepQAgent(
+    env=<Monitor<FrameStackEnv<ClipRewardEnv<PenalizeDeathEnv<DownsampleEnv<FireResetEnv<MaxFrameskipEnv<NoopResetEnv<TimeLimit<AtariEnv<BreakoutNoFrameskip-v4>>>>>>>>>>>,
+    render_mode='rgb_array'
+    replay_memory_size=1000000,
+    discount_factor=0.99,
+    update_frequency=4,
+    optimizer=<keras.optimizers.Adam object at 0x2ab10e0480f0>,
+    exploration_rate=AnnealingVariable(initial_value=1.0, final_value=0.1, steps=1000000),
+    loss=huber_loss,
+    target_update_freq=10000,
+    dueling_network=True
+)
