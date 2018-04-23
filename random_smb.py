8ecdc5b9b9a4ea8416f3f40a3b2fc9f4b5f68b7c
Kautenja <kautencreations@gmail.com>
2018-04-23 01:23:03 -0500

    update

--- random_smb.py
+++ random_smb.py
@@ -1,4 +1,5 @@
 from multiprocessing import Lock
+from tqdm import tqdm
 import pandas as pd
 from gym.wrappers import Monitor
 from src.environment.nes import build_nes_environment
@@ -10,12 +11,13 @@ env.configure(lock=Lock())
 
 scores = []
 
-for game in range(100):
+for game in tqdm(range(100)):
     env.reset()
     done = False
     score = 0
     while not done:
         _, reward, done, _ = env.step(env.action_space.sample())
+        score += reward
         env.render()
     scores.append(score)
 
