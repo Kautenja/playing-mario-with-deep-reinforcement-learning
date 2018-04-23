d7c856db45a7eb9a2f8154b3b27bc2b3adfb4de2
Kautenja <ckfour@icloud.com>
2018-04-23 17:03:13 -0500

    update DDDQN play

--- dddqn_play.py
+++ dddqn_play.py
@@ -1,52 +1,59 @@
 """Usage:
 
-    python play.py <results directory>
+    python dddqn_play.py <results directory>
 """
 import os
 import sys
 import pandas as pd
 from gym.wrappers import Monitor
-from src.agents import DeepQAgent, A3CAgent
-from src.environment.atari import build_atari_environment
-
-
-# a mapping of string names to agents
-agents = {
-    DeepQAgent.__name__: DeepQAgent,
-    A3CAgent.__name__: A3CAgent,
-}
 
 
 # load variables from the command line
 try:
-    exp_directory = sys.argv[1]
-    dirs = exp_directory.split('/')
-    agent_name = dirs[-2]
-    game = dirs[-3]
+    output_dir = sys.argv[1]
+    # these will have the form .../<game_name>/<method>/<timestamp>
+    game_name = output_dir.split('/')[-3]
 except IndexError:
     print(__doc__)
     sys.exit(-1)
-# set up the weights file
-weights_file = '{}/weights.h5'.format(exp_directory)
 
+
+# set up the weights file
+weights_file = '{}/weights.h5'.format(output_dir)
 # make sure the weights exist
 if not os.path.exists(weights_file):
     print('{} not found!'.format(weights_file))
     sys.exit(-1)
 
 
-# build the environment
-env, r_cache = build_atari_environment(game, is_validation=True)
-# env = Monitor(env, '{}/monitor_play'.format(exp_directory), force=True)
-# build the agent without any replay memory (not needed to play from model)
-agent = agents[agent_name](env, replay_memory_size=0)
-# load the weights
+# load these after command line arg checking bc tensorflow is slow to load
+# and generates some warning output
+from src.environment.atari import build_atari_environment
+from src.environment.nes import build_nes_environment
+from src.agents import DeepQAgent
+
+
+# check if we need to load the NES environment
+if 'SuperMarioBros' in game_name:
+    env = build_nes_environment(game_name)
+# default to the Atari environment
+else:
+    env = build_atari_environment(game_name, is_validation=True)
+# wrap the environment with a monitor
+env = Monitor(env, '{}/monitor_play'.format(output_dir), force=True)
+
+
+# build the agent without any replay memory since we're just playing, load
+# the trained weights, and play some games
+agent = DeepQAgent(env, replay_memory_size=0)
 agent.model.load_weights(weights_file)
-# play some games
+agent.target_model.load_weights(weights_file)
 agent.play()
+
+
 # collect the game scores
-scores = pd.Series(r_cache._rewards)
-scores.to_csv('{}/final_validation.csv'.format(exp_directory))
+scores = pd.Series(env.unwrapped.episode_rewards)
+scores.to_csv('{}/final_scores.csv'.format(output_dir))
 # print some stats
 print('min ', scores.min())
 print('mean ', scores.mean())
