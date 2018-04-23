7397c9b40b978bfd33c451e238a5c3eb744cd81e
Kautenja <kautencreations@gmail.com>
2018-04-22 20:53:35 -0500

    update

---
+++ results/SuperMarioBros-1-1/DeepQAgent/2018-04-22_20-11/agent.py
@@ -0,0 +1,12 @@
+DeepQAgent(
+    env=<FrameStackEnv<ClipRewardEnv<PenalizeDeathEnv<DownsampleEnv<ToDiscreteWrapper<TimeLimit<SuperMarioBrosEnv instance>>>>>>>,
+    render_mode='rgb_array'
+    replay_memory_size=750000,
+    discount_factor=0.99,
+    update_frequency=4,
+    optimizer=<keras.optimizers.Adam object at 0x7f03f72b2860>,
+    exploration_rate=AnnealingVariable(initial_value=0.1, final_value=0.1, steps=1),
+    loss=huber_loss,
+    target_update_freq=10000,
+    dueling_network=True
+)
