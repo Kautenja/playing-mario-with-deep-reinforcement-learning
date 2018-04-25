955d4c11c566177acb2dad033c474376cff57fab
Kautenja <ckfour@icloud.com>
2018-04-24 21:55:31 -0500

    update to use nesgym-super-mario-bros

--- src/environment/nes.py
+++ src/environment/nes.py
@@ -5,17 +5,13 @@ from src.environment.wrappers import (
     DownsampleEnv,
     FrameStackEnv,
     NoopResetEnv,
-)
-from super_mario.wrappers import (
-    ToDiscreteActionSpaceEnv,
-    PenalizeDeathEnv,
     RewardCacheEnv
 )
+import nesgym_super_mario_bros
 
 
 def build_nes_environment(game_name: str,
     image_size: tuple=(84, 84),
-    death_penalty: int=-1,
     clip_rewards: bool=True,
     agent_history_length: int=4,
 ):
@@ -24,11 +20,7 @@ def build_nes_environment(game_name: str,
 
     Args:
         game_name: the name of the NES game to make
-        is_validation: whether to use the validation or training environment
         image_size: the size to down-sample images to
-        noop_max: the max number of random no-ops at the beginning of a game
-        skip_frames: the number of frames to hold each action for
-        death_penatly: the penalty for losing a life in a game
         clip_rewards: whether to clip rewards in {-1, 0, +1}
         agent_history_length: the size of the frame buffer for the agent
 
@@ -37,17 +29,12 @@ def build_nes_environment(game_name: str,
 
     """
     # make the initial environment
-    env = gym.make('{}-v0'.format(game_name))
-    # convert the action space to a discrete action space
-    env = ToDiscreteActionSpaceEnv(env)
+    env = gym.make('nesgym/{}-v0'.format(game_name))
     # add a reward cache for scoring episodes
     env = RewardCacheEnv(env)
     # apply a down-sampler for the given game
     downsampler = DownsampleEnv.metadata[game_name.split('-')[0]]
     env = DownsampleEnv(env, image_size, **downsampler)
-    # apply the death penalty feature if enabled
-    if death_penalty is not None:
-        env = PenalizeDeathEnv(env, penalty=death_penalty)
     # clip the rewards in {-1, 0, +1} if the feature is enabled
     if clip_rewards:
         env = ClipRewardEnv(env)
