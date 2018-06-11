7c1ebcebed7327a9e1c7db765c2c2501da955c70
Kautenja <kautencreations@gmail.com>
2018-06-11 14:06:24 -0500

    preliminary prioritized experience replay

---
+++ src/base/prioritized_replay_queue.py
@@ -0,0 +1,91 @@
+"""A priority queue for storing previous experiences to sample from."""
+from heapq import heappush
+import numpy as np
+
+
+class PrioritizedReplayQueue(object):
+    """A prioritized replay queue for replaying previous experiences."""
+
+    def __init__(self, size: int) -> None:
+        """
+        Initialize a new prioritized replay buffer with a given size.
+
+        Args:
+            size: the size of the replay buffer
+                  (the number of previous experiences to store)
+
+        Returns:
+            None
+
+        """
+        # type check the size parameter
+        if not isinstance(size, int):
+            raise TypeError('`size` must be of type int')
+        # ensure the size is within a legal range of values
+        if size <= 0:
+            raise ValueError('`size` must be > 0')
+        self.size = size
+        # initialize the priority queue as a heap
+        self.heap = []
+
+    def __repr__(self) -> str:
+        """Return an executable string representation of priority queue."""
+        return '{}(size={})'.format(self.__class__.__name__, self.size)
+
+    def push(self, *args, priority: float) -> None:
+        """
+        Push a new experience onto the queue.
+
+        Args:
+            *args: the experience s, a, r, d, s2
+            priority: the priority of the item to push to the queue
+
+        Returns:
+            None
+
+        """
+        # push the variable onto the heap
+        heappush(self.heap, (priority, args))
+        # bound the heap within the size limit
+        self.heap = self.heap[:self.size]
+
+    def sample(self, size: int=32) -> bool:
+        """
+        Return a random sample of items from the queue.
+
+        Args:
+            size: the number of items to sample and return
+
+        Returns:
+            A random sample from the queue sampled uniformly
+
+        """
+        # extract a sample from the heap (priorities are in increasing order)
+        # i.e. the lowest priority value is the first item in the sample
+        sample_batch = [x[1] for x in self.heap[:size]]
+        # initialize lists for each component of the batch
+        s = [None] * len(sample_batch)
+        a = [None] * len(sample_batch)
+        r = [None] * len(sample_batch)
+        d = [None] * len(sample_batch)
+        s2 = [None] * len(sample_batch)
+        # iterate over the indexes and copy references to the arrays
+        for batch, sample in enumerate(sample_batch):
+            _s, _a, _r, _d, _s2 = sample
+            s[batch] = np.array(_s, copy=False)
+            a[batch] = _a
+            r[batch] = _r
+            d[batch] = _d
+            s2[batch] = np.array(_s2, copy=False)
+        # convert the lists to arrays for returning for training
+        return (
+            np.array(s),
+            np.array(a, dtype=np.uint8),
+            np.array(r, dtype=np.int8),
+            np.array(d, dtype=np.bool),
+            np.array(s2),
+        )
+
+
+# explicitly define the outward facing API of this module
+__all__ = [PrioritizedReplayQueue.__name__]
