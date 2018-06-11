8730608efbd68948dd4e095060e5159445e3170d
Kautenja <kautencreations@gmail.com>
2018-06-11 15:13:46 -0500

    priority experience queue in base package

--- src/base/__init__.py
+++ src/base/__init__.py
@@ -1,10 +1,12 @@
 """Base components for the project."""
 from .annealing_variable import AnnealingVariable
+from .prioritized_replay_queue import PrioritizedReplayQueue
 from .replay_queue import ReplayQueue
 
 
 # explicitly define the outward facing API for the package.
 __all__ = [
     AnnealingVariable.__name__,
-    ReplayQueue.__name__
+    PrioritizedReplayQueue.__name__,
+    ReplayQueue.__name__,
 ]
