9b76d0c810f9673f48d8990414ad9aff20c32534
Christian Kauten <Kautenja@users.noreply.github.com>
2018-06-14 00:30:35 -0500

    Update __main__.py
    fix matplotlib for servers

--- __main__.py
+++ __main__.py
@@ -1,6 +1,8 @@
 """The main execution script for this package."""
+# set matplotlib to override default X11 environment
+import matplotlib
+matplotlib.use('Agg')
+# import the main entry point of the application
 from src.cli import main
-
-
 # execute the main entry point of the CLI
 main()
