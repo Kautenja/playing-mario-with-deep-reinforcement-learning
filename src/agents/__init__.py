1124816451a51576856b7af4b1f274f88b00a8ac
Kautenja <kautencreations@gmail.com>
2019-01-12 16:35:56 -0600

    export Agent class

--- src/agents/__init__.py
+++ src/agents/__init__.py
@@ -1,10 +1,12 @@
 """A package with implementations of deep reinforcement agents."""
+from .agent import Agent
 from .random_agent import RandomAgent
 from .deep_q_agent import DeepQAgent
 
 
 # explicitly define the outward facing API of this package.
 __all__ = [
+    Agent.__name__,
     RandomAgent.__class__,
     DeepQAgent.__class__,
 ]
