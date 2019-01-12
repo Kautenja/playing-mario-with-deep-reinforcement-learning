9a47baacd5a48831669c57da4c831cd91f3f2acb
Kautenja <kautencreations@gmail.com>
2019-01-12 16:00:11 -0600

    cleanup to be more general

--- scripts/random_agent.py
+++ scripts/random_agent.py
@@ -2,11 +2,11 @@
 import datetime
 import os
 import shutil
-import _top_level
 import gym
 import gym_super_mario_bros
 from nes_py.wrappers import BinarySpaceToDiscreteSpaceEnv, wrap as nes_py_wrap
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+import _top_level
 from src.agents import RandomAgent
 from src.training import stochastic
 from src.utils import log_
@@ -46,6 +46,29 @@ def experiment_dir(output_dir: str, env_id: str, agent_name: str) -> str:
     return output_dir
 
 
+def getattr_and_call(agent_: object, method_: str, groups) -> any:
+    """
+    Lookup a method in an agent and call it with command line arguments.
+
+    Args:
+        agent_: the agent to lookup a method in
+        method_: the method to lookup in the agent
+        groups: the argument groups from argparser to get keyword args from
+
+    Returns:
+        the return of the provided method if the method exists, else None
+
+    """
+    # if the agent doesn't have this feature, return None
+    if not hasattr(agent_, method_):
+        return None
+    # if the method isn't feature in the command line args, call with defaults
+    if method_ not in groups:
+        return getattr(agent_, method_)()
+    # get the method and call it with the given arguments
+    return getattr(agent_, method_)(**vars(groups[method_]))
+
+
 @stochastic(ARGS.seed, ARGS.gpu)
 def main():
     """Train the model."""
@@ -65,11 +88,11 @@ def main():
     agent_kwargs = vars(ARG_GROUPS['agent'])
     agent_kwargs['env'] = env
     agent = RandomAgent(**vars(ARG_GROUPS['agent']))
-    log_('Random Agent', agent)
-
-    # agent.observe(**vars(ARG_GROUPS['observe']))
-    # agent.train(**vars(ARG_GROUPS['train']))
-    agent.play(**vars(ARG_GROUPS['play']))
+    log_('Agent', agent)
+    # get the observe, train, and play methods and call them
+    getattr_and_call(agent, 'observe', ARG_GROUPS)
+    getattr_and_call(agent, 'train', ARG_GROUPS)
+    getattr_and_call(agent, 'play', ARG_GROUPS)
 
 
 if __name__ == '__main__':
