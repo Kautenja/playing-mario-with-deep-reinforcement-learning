67a9a75707a2539239e85ee6eb229af827fa7c2b
Kautenja <kautencreations@gmail.com>
2019-01-12 19:10:48 -0600

    export agents module

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,8 @@
 """Source code to train agents to play Super Mario Bros."""
+from . import agents
+
+
+# explicitly define the outward facing API of this module
+__all__ = [
+    agents.__name__,
+]
