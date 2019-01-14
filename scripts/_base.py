e8eaab7c656796565f7d2b0bfc6b6847a846b591
Kautenja <ckfour@icloud.com>
2019-01-14 11:30:10 -0600

    update to handle monitor

--- scripts/_base.py
+++ scripts/_base.py
@@ -2,6 +2,7 @@
 import datetime
 import os
 import shutil
+import gym
 import gym_super_mario_bros
 from nes_py.wrappers import BinarySpaceToDiscreteSpaceEnv, wrap as nes_py_wrap
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
@@ -32,12 +33,13 @@ def experiment_dir(output_dir: str, env_id: str, agent_name: str) -> str:
     return output_dir
 
 
-def build_env(env: str) -> 'gym.Env':
+def build_env(env: str, monitor_dir: str=None) -> 'gym.Env':
     """
     Build an environment for an agent.
 
     Args:
         env: the name of the environment to make
+        monitor_dir: the directory to output monitor data to
 
     Returns:
         a gym.Env based on the given env ID with the appropriate wrappers
@@ -45,6 +47,8 @@ def build_env(env: str) -> 'gym.Env':
     """
     env = gym_super_mario_bros.make(env)
     env = BinarySpaceToDiscreteSpaceEnv(env, SIMPLE_MOVEMENT)
+    if monitor_dir is not None:
+        env = gym.wrappers.Monitor(env, monitor_dir, force=True)
     env = nes_py_wrap(env)
 
     return env
