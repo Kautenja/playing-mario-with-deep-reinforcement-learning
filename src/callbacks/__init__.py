8a0168b3930e4fc9990a3d18c4f57d94c0a30c37
Kautenja <kautencreations@gmail.com>
2019-01-20 03:55:25 -0600

    move callbacks to dedicated package

---
+++ src/callbacks/__init__.py
@@ -0,0 +1,10 @@
+"""Callback methods for the agent `train` methods."""
+from .base_callback import BaseCallback
+from .jupyter_callback import JupyterCallback
+
+
+# explicitly define the outward facing API of this package
+__all__ = [
+    BaseCallback.__name__,
+    JupyterCallback.__name__,
+]
